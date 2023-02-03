fb078bbcbef2055eed5058f178c286ac335bdc2c
Riyaz <mohriyaz17@gmail.com>
2023-02-03 02:40:48 -0800

    fetching particular id by req.params

---
+++ backend/data/notes.js
@@ -0,0 +1,31 @@
+const notes = [
+    {
+        id:"1",
+        title:"Day1 of college",
+        content:"I made new friends",
+        category:"college"
+    },
+    {
+        id:"2",
+        title:"Day2 of college",
+        content:"I made old lerining",
+        category:"lerning"
+    },
+    {
+        id:"3",
+        title:"Day3 of college",
+        content:"I made broke friends",
+        category:"lerning"
+    },
+    {
+        id:"4",
+        title:"Day4 of college",
+        content:"I made new and old friends",
+        category:"college"
+    },
+
+]
+
+module.exports = notes;
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const notes = require("./data/notes");
 const app = express();
 const PORT = 3000;
 
-app.get("/",(req,res)=>{
-    res.send("API is running..");
-})
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
 
-app.listen(PORT,()=>{
-    console.log("Server is running",PORT);
-})
+app.get("/api/notes", (req, res) => {
+
+  res.json(notes);
+  console.log(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+    
+  const note = notes.find((n) => n.id === req.params.id);
+  res.send(note);
+  console.log(req.params);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running", PORT);
+});
