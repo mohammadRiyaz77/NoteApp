066a1decc4fb3174c9f0b5ab7c242883afa10761
Riyaz <mohriyaz17@gmail.com>
2023-02-03 03:19:44 -0800

    initalize front and backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get("/api/notes/:id", (req, res) => {
     // console.log(req.params);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
