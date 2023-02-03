2aec80e9f6c7eb12da985b4913f739eda1f5393e
Riyaz <mohriyaz17@gmail.com>
2023-02-03 02:56:24 -0800

    cahnges related to env file

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const notes = require("./data/notes");
 const app = express();
-const PORT = 3000;
+dotenv.config(); //  .env file its give securtiy or secret information to keep hidden
 
 app.get("/", (req, res) => {
-  res.send("API is running..");
+    res.send("API is running..");
 });
 
 app.get("/api/notes", (req, res) => {
-
-  res.json(notes);
-  console.log(notes);
+    res.json(notes);
+    console.log(notes);
 });
 
 app.get("/api/notes/:id", (req, res) => {
     
-  const note = notes.find((n) => n.id === req.params.id);
-  res.send(note);
-  console.log(req.params);
+    const note = notes.find((n) => n.id === req.params.id);
+    res.send(note);
+    // console.log(req.params);
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is running", PORT);
+  console.log(`Server is running on ${PORT}`);
 });
